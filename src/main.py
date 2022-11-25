76aa9a45e37ecc52323bdd4c64e027ef684c56b6
melvinkj <melvinkentj@gmail.com>
2022-11-25 22:15:37 +0700

    Add color to splash screen

--- src/main.py
+++ src/main.py
@@ -9,13 +9,13 @@ from termcolor import colored
 
 
 def splash_screen():
-    print(colored("    888                            dP\"8                 ,e,            d8    888 88e                                       ", "red"))
-    print(colored("    888  ,\"Y88b Y8b Y888P  ,\"Y88b C8b Y  e88'888 888,8,  \"  888 88e   d88    888 888D  ,\"Y88b 888,8,  dP\"Y  ,e e,  888,8, ", "yellow"))
-    print(colored("    888 \"8\" 888  Y8b Y8P  \"8\" 888  Y8b  d888  '8 888 \"  888 888 888b d88888  888 88\"  \"8\" 888 888 \"  C88b  d88 88b 888 \"  ", "green"))
-    print(colored(" e  88P ,ee 888   Y8b \"   ,ee 888 b Y8D Y888   , 888    888 888 888P  888    888      ,ee 888 888     Y88D 888   , 888    ", "cyan"))
-    print(colored("\"8\",P'  \"88 888    Y8P    \"88 888 8edP   \"88,e8' 888    888 888 88\"   888    888      \"88 888 888    d,dP   \"YeeP\" 888    ", "blue"))
-    print(colored("                                                            888                                                           ", "magenta"))
-    print(colored("                                                            888                                                             ", "magenta"))     
+    print("\033[91m    888                            dP\"8                 ,e,            d8    888 88e                                       \033[0m")
+    print("\033[93m    888  ,\"Y88b Y8b Y888P  ,\"Y88b C8b Y  e88'888 888,8,  \"  888 88e   d88    888 888D  ,\"Y88b 888,8,  dP\"Y  ,e e,  888,8, \033[0m")
+    print("\033[92m    888 \"8\" 888  Y8b Y8P  \"8\" 888  Y8b  d888  '8 888 \"  888 888 888b d88888  888 88\"  \"8\" 888 888 \"  C88b  d88 88b 888 \"  \033[0m")
+    print("\033[96m e  88P ,ee 888   Y8b \"   ,ee 888 b Y8D Y888   , 888    888 888 888P  888    888      ,ee 888 888     Y88D 888   , 888    \033[0m")
+    print("\033[94m\"8\",P'  \"88 888    Y8P    \"88 888 8edP   \"88,e8' 888    888 888 88\"   888    888      \"88 888 888    d,dP   \"YeeP\" 888    \033[0m")
+    print("\033[95m                                                            888                                                           \033[0m")
+    print("\033[95m                                                            888                                                             \033[0m")     
 
 def main():
     program_parser = argparse.ArgumentParser(description='Accept or reject javascript syntax.')
