c4b9cf364768110232a1ca12a31e706ab84da335
melvinkj <melvinkentj@gmail.com>
2022-11-25 08:18:33 +0700

    feat: Add splash screen

--- src/main.py
+++ src/main.py
@@ -6,7 +6,16 @@ from lib.tokenizer import createToken
 from lib.CFGtoCNF import readGrammarFile, convertGrammar, mapGrammar
 from lib.CYKparser import cykParse
 
-
+def splash_screen():
+    print()
+    print("    888                              e88'Y88                                ,e, 888                ")
+    print("    888  ,\"Y88b Y8b Y888P  ,\"Y88b   d888  'Y  e88 88e  888 888 8e  888 88e   \"  888  ,e e,  888,8, ")
+    print("    888 \"8\" 888  Y8b Y8P  \"8\" 888  C8888     d888 888b 888 888 88b 888 888b 888 888 d88 88b 888 \"  ")
+    print(" e  88P ,ee 888   Y8b \"   ,ee 888   Y888  ,d Y888 888P 888 888 888 888 888P 888 888 888   , 888    ")
+    print("\"8\",P'  \"88 888    Y8P    \"88 888    \"88,d88  \"88 88\"  888 888 888 888 88\"  888 888  \"YeeP\" 888    ")
+    print("                                                                   888                             ")
+    print("                                                                   888                             ")
+        
 
 def main():
     program_parser = argparse.ArgumentParser(description='Accept or reject javascript syntax.')
@@ -24,6 +33,12 @@ def main():
     if not os.path.isfile(input_file):
         print("The file doesn't exist.")
         sys.exit()
+
+    splash_screen()
+    print("Processing...")
+    print("Analyzing your file...")
+    print("File name: " + str(input_file))
+
 
     # print('\n'.join(os.listdir(input_file)))
 
